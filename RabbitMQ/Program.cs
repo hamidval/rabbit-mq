b76060f39cd85050891896b5ceea7cb43fbca65d
hamidval <44161836+hamidval@users.noreply.github.com>
2023-10-02 15:03:56 +0100

    adding project

---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+Timer timer1;
+Console.WriteLine("Hello, World!");
+
+//Sender
+ConnectionFactory factory = new ConnectionFactory();
+
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit sender app";
+
+IConnection connection = factory.CreateConnection();
+
+IModel channel = connection.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+
+
+timer1 = new Timer(TimerCallback, null, 0, (int) 1000);
+
+Console.ReadLine();
+
+void TimerCallback(Object stateInfo) 
+{
+    channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+    channel.QueueDeclare(queueName, false, false, false, null);
+    channel.QueueBind(queueName, exchangeName, routingKey);
+
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes("hello youtube");
+    channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+
+   // channel.Close();
+   // connection.Close();
+}
---
+++ RabbitReciever1/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Sender
+ConnectionFactory factory = new ConnectionFactory();
+
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit reciever 1 app";
+
+IConnection connection = factory.CreateConnection();
+
+IModel channel = connection.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey);
+
+channel.BasicQos(0, 1, false);
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, args) =>
+{
+    var body = args.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+string consumerTag = channel.BasicConsume(queueName, false, consumer);
+Console.ReadLine();
+channel.BasicCancel(consumerTag);
+
+channel.Close();
+connection.Close();
